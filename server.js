ff089308da0336a3275c731e71a1bd8fd7c96495
rastogi-s <35542305+rastogi-s@users.noreply.github.com>
2018-06-20 12:30:16 -0400

    heroku changes

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/white-client-angular'));
+app.use(express.static(path.join(__dirname, '/dist/white-client-angular')));
 
 app.get('/*', function(req,res) {
 
-  res.sendFile(path.join(__dirname,'/dist/whiteboard-client-angular/index.html'));
+  res.sendFile(express.static(path.join(__dirname,'/dist/whiteboard-client-angular/index.html')));
 });
 
 // Start the app by listening on the default Heroku port
