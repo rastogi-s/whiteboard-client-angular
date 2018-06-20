073462ec4a6599ce35f5a3b4013f7f95a2c7f91e
rastogi-s <35542305+rastogi-s@users.noreply.github.com>
2018-06-19 23:04:12 -0400

    heroku deploy changes

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.static('./dist/white-client-angular'));
 
 app.get('/*', function(req,res) {
 
-  res.sendFile(path.join(__dirname,'/dist/your_app_name/index.html'));
+  res.sendFile(path.join(__dirname,'/dist/whiteboard-client-angular/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
